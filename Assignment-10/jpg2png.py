4e7f2f6c24baf2d9355d636b17a5807f9b03dad5
amirhosseinhajian <ahhnd99@gmail.com>
2023-06-14 16:27:15 +0430

    improve algorithm

--- Assignment-10/jpg2png.py
+++ Assignment-10/jpg2png.py
@@ -1,11 +1,9 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("./inputs/microsoft.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA)
-filter_arr = img % 2 == 0
-for i, row in enumerate(img):
-    for j, pixle in enumerate(row):
-        b, g, r, a = img[i][j]
-        if 70<b<100 and 70<g<100 and 70<r<100:
-            img[i][j][3] = 0
+img = cv2.cvtColor(img, cv2.COLOR_BGR2BGRA).astype(np.float32)
+img[(70<img[:, :, 0]) &(img[:, :, 0]<100) & (70<img[:, :, 1]) & (img[:, :, 1]<100) & (70<img[:, :, 2]) & (img[:, :, 2]<100)]\
+      *= (1, 1, 1, 0)
+img = img.astype(np.uint8)
 cv2.imwrite("./outputs/microsof_logo.png", img)
